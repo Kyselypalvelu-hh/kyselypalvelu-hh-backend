718e1feed819f76432579a07c9fa244f2caad4fc
Jouni Johansson <bgu211@myy.haaga-helia.fi>
2022-10-27 12:59:29 +0300

    first commit

---
+++ src/main/java/swd22/hh/fi/hhkyselypalvelu/HhKyselypalveluApplication.java
@@ -0,0 +1,13 @@
+package swd22.hh.fi.hhkyselypalvelu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HhKyselypalveluApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HhKyselypalveluApplication.class, args);
+	}
+
+}
---
+++ src/test/java/swd22/hh/fi/hhkyselypalvelu/HhKyselypalveluApplicationTests.java
@@ -0,0 +1,13 @@
+package swd22.hh.fi.hhkyselypalvelu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HhKyselypalveluApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
