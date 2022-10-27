32cd35c8f40ec92edd97dddac4f222e110382add
Lauri Iivarinen <lauri.iivarinen@gmail.com>
2022-10-27 13:22:05 +0300

    index controller

--- src/main/java/swd22/hh/fi/hhkyselypalvelu/web/IndexController.java
+++ src/main/java/swd22/hh/fi/hhkyselypalvelu/web/IndexController.java
@@ -1,5 +1,13 @@
 package swd22.hh.fi.hhkyselypalvelu.web;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class IndexController {
 
+	@GetMapping("/")
+	public String getIndex() {
+		return "index";
+	}
 }
