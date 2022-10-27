c459bb1c3dac1832e1e22e85667f1bebbd8fb319
Lauri Iivarinen <lauri.iivarinen@gmail.com>
2022-10-27 13:47:03 +0300

    controller endpoinnteja + questionamount.html alotus NYI

--- src/main/java/swd22/hh/fi/hhkyselypalvelu/web/IndexController.java
+++ src/main/java/swd22/hh/fi/hhkyselypalvelu/web/IndexController.java
@@ -1,13 +1,28 @@
 package swd22.hh.fi.hhkyselypalvelu.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class IndexController {
 
+	//FRONT PAGE
 	@GetMapping("/")
 	public String getIndex() {
 		return "index";
 	}
+	
+	@GetMapping("/questionamount")
+	public String getQuestionAmount(Model model) {
+		return "questionamount";
+	}
+	
+	
+	//FORM TO CREATE NEW QUERIES(FORMS) REQUIRES AMOUNT OF QUESTIONS
+	@GetMapping("/createform")
+	public String getCreateform() {
+		return "createform";
+	}
+
 }
