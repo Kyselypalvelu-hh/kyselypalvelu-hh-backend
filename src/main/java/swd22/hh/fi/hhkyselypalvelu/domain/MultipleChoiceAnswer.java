4ac5b8dd3fb61a7543bd49af4b1466d7a5732a6e
Lauri Iivarinen <lauri.iivarinen@gmail.com>
2022-11-10 11:57:26 +0200

    multiplechoice answer started

--- src/main/java/swd22/hh/fi/hhkyselypalvelu/domain/MultipleChoiceAnswer.java
+++ src/main/java/swd22/hh/fi/hhkyselypalvelu/domain/MultipleChoiceAnswer.java
@@ -4,10 +4,21 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 
-
+@Entity
 public class MultipleChoiceAnswer {
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long answerId;
+	
+	@ManyToOne
+	@JoinColumn(name="optionId")
+	private MultipleChoiceOption option;
+	
+	private MultipleChoiceQuestion question = option.getQuestion();
 	
 	
 }
--- src/main/java/swd22/hh/fi/hhkyselypalvelu/domain/MultipleChoiceOption.java
+++ src/main/java/swd22/hh/fi/hhkyselypalvelu/domain/MultipleChoiceOption.java
@@ -1,11 +1,15 @@
 package swd22.hh.fi.hhkyselypalvelu.domain;
 
+import java.util.List;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
@@ -22,6 +26,10 @@ public class MultipleChoiceOption {
 	@JoinColumn(name="questionId")
 	@JsonIgnoreProperties("choiceOptions")
 	private	MultipleChoiceQuestion question;
+	
+	@OneToMany(cascade = CascadeType.ALL, mappedBy = "option")
+	@JsonIgnoreProperties("option")
+	private List<MultipleChoiceAnswer> answers;
 	
 	
 	//CONSTRUCTORS
