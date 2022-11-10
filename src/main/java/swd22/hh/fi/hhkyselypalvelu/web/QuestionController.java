ebeb4549d175fac0b90dc250729351cc52fd66fc
Lauri Iivarinen <lauri.iivarinen@gmail.com>
2022-11-10 13:20:47 +0200

    jouni

--- src/main/java/swd22/hh/fi/hhkyselypalvelu/web/QuestionController.java
+++ src/main/java/swd22/hh/fi/hhkyselypalvelu/web/QuestionController.java
@@ -16,6 +16,7 @@ import swd22.hh.fi.hhkyselypalvelu.domain.OpenTextQuestionRepository;
 @Controller // Controller for questions made with thymeleaf
 public class QuestionController {
 	
+	//
 	@Autowired
 	private OpenTextQuestionRepository questionrepo;
 	@Autowired
