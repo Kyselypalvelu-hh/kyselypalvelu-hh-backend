e90554782d19cacd7d71d2b41bbb936817bbc7db
Jouni Johansson <bgu211@myy.haaga-helia.fi>
2022-11-10 14:16:49 +0200

    thymeleaf multiplechoice incomiiing

--- src/main/java/swd22/hh/fi/hhkyselypalvelu/web/QuestionController.java
+++ src/main/java/swd22/hh/fi/hhkyselypalvelu/web/QuestionController.java
@@ -1,5 +1,6 @@
 package swd22.hh.fi.hhkyselypalvelu.web;
 
+import org.aspectj.weaver.patterns.TypePatternQuestions.Question;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -10,6 +11,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 
 import swd22.hh.fi.hhkyselypalvelu.domain.Query;
 import swd22.hh.fi.hhkyselypalvelu.domain.QueryRepository;
+import swd22.hh.fi.hhkyselypalvelu.domain.MultipleChoiceAnswer;
+import swd22.hh.fi.hhkyselypalvelu.domain.MultipleChoiceQuestion;
+import swd22.hh.fi.hhkyselypalvelu.domain.MultipleChoiceQuestionRepository;
 import swd22.hh.fi.hhkyselypalvelu.domain.OpenTextQuestion;
 import swd22.hh.fi.hhkyselypalvelu.domain.OpenTextQuestionRepository;
 
@@ -21,16 +25,20 @@ public class QuestionController {
 	private OpenTextQuestionRepository questionrepo;
 	@Autowired
 	private QueryRepository queryrepo;
-	
+	@Autowired
+	private MultipleChoiceQuestionRepository multichoicequestionrepo;
 	
 	//RECEIVES EMPTY QUESTION 
 	@GetMapping("/createquestions/{id}")
 	public String getQuestionCreation(@PathVariable("id") Long id,Model model) {
 		OpenTextQuestion textquestion = new OpenTextQuestion();
+		MultipleChoiceQuestion multichoicequestion = new MultipleChoiceQuestion();
 		Query query = queryrepo.findById(id).get();
 		textquestion.setQuery(query);
+		multichoicequestion.setQuery(query);
 		model.addAttribute("question", textquestion);
 		model.addAttribute("questions", query.getTextQuestions());
+		model.addAttribute("multichoisequestion", multichoicequestion);
 		return "createquestion";
 	}
 	
@@ -41,5 +49,19 @@ public class QuestionController {
 		questionrepo.save(question);		
 		return "redirect:/createquestions/"+question.getQuery().getId(); // redirect to add new questions
 	}
-
+	
+	@PostMapping("/savemultichoicequestion")
+	public String saveMultiChoiceQuestion(@ModelAttribute MultipleChoiceQuestion multiquestion, Model model) {
+		
+		multichoicequestionrepo.save(multiquestion);		
+		return "redirect:/createmultichoiceanswers/"+multiquestion.getQuestionId(); // redirect to add new questions
+	}
+	/**
+	@GetMapping("/createmultichoiceanswers/{id}")
+	public String getMultipleChoiceAnswerCreation(@PathVariable("id") Long id,Model model) {
+		MultipleChoiceAnswer multianswers = new MultipleChoiceAnswer();
+		MultipleChoiceQuestion multiquestion = multichoicequestionrepo.findById(id).get();
+		model.addAttribute("answers", multianswers);
+		return "addmultianswers";
+	}**/
 }
